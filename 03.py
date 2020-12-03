51d577dd346e2fb03fbf9e23c7a6a8df237884ff
Michael Fogleman <fogleman@gmail.com>
2020-12-03 10:31:47 -0500

    day 3

---
+++ 03.py
@@ -0,0 +1,24 @@
+import fileinput
+import functools
+import operator
+
+def hits(grid, dx, dy):
+    w = len(grid[0])
+    h = len(grid)
+    x = y = count = 0
+    while y < h:
+        if grid[y][x%w] == '#':
+            count += 1
+        x += dx
+        y += dy
+    return count
+
+grid = [x.strip() for x in fileinput.input()]
+
+# part 1
+print(hits(grid, 3, 1))
+
+# part 2
+dirs = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+counts = [hits(grid, dx, dy) for dx, dy in dirs]
+print(functools.reduce(operator.mul, counts))
