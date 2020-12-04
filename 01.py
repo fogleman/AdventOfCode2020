20ecb7ed2099daabd2fa82eb2dcfab34547c1a35
Michael Fogleman <fogleman@gmail.com>
2020-12-03 20:30:08 -0500

    Use math.prod

--- 01.py
+++ 01.py
@@ -1,13 +1,12 @@
 import fileinput
-import functools
 import itertools
-import operator
+import math
 
 def search(values, total, count):
     for group in itertools.combinations(values, count - 1):
         need = total - sum(group)
         if need in values:
-            return functools.reduce(operator.mul, group, need)
+            return math.prod(group, start=need)
 
 values = set(map(int, fileinput.input()))
 print(search(values, 2020, 2))
