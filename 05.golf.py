d47025b0c8b3fc3c0be8b22eac9854309e6dffe2
Michael Fogleman <fogleman@gmail.com>
2020-12-05 10:12:08 -0500

    golf

--- 05.golf.py
+++ 05.golf.py
@@ -1,5 +1,6 @@
 import fileinput
 
-ids = set(int(''.join(str(int(not ord(c) & 4)) for c in x[:10]), 2)
+ids = set(sum(('FBLR'.index(x[9-i])&1)<<i for i in range(10))
     for x in fileinput.input())
-print(max(ids), set(range(min(ids), max(ids) + 1)) - ids)
+
+print(max(ids), set(range(min(ids), max(ids))) - ids)
