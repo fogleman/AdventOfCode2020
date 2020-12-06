2d634b0ee656321a0d54349c73118d9276530d17
Michael Fogleman <fogleman@gmail.com>
2020-12-06 09:25:09 -0500

    day 6

--- 06.py
+++ 06.py
@@ -1,7 +1,6 @@
 import fileinput
 
-groups = [list(map(set, x.split()))
-    for x in ''.join(fileinput.input()).split('\n\n')]
+groups = [x.split() for x in ''.join(fileinput.input()).split('\n\n')]
 
 for f in [set.union, set.intersection]:
-    print(sum(len(f(*g)) for g in groups))
+    print(sum(len(f(*map(set, g))) for g in groups))
