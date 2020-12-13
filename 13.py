83f9a6d6d3db735b9adab9a1be004b5ea91ee96b
Michael Fogleman <fogleman@gmail.com>
2020-12-13 17:03:05 -0500

    clean up

--- 13.py
+++ 13.py
@@ -5,23 +5,20 @@ lines = list(fileinput.input())
 t0 = int(lines[0])
 rules = [(i, int(x)) for i, x in
     enumerate(lines[1].split(',')) if x.isdigit()]
-ids = [x[1] for x in rules]
 
 # part 1
-print(math.prod(min(((x - t0 % x) % x, x) for x in ids)))
+print(math.prod(min((x - t0 % x, x)
+    for x in [rule[1] for rule in rules])))
 
 # part 2
-def solve(rules, t, dt):
-    prev = result = 0
-    while True:
-        if all(t % b == (b - a) % b for a, b in rules):
-            result = result or t
-            if prev:
-                return result, t - prev
-            prev = t
-        t += dt
-
 t, dt = 0, 1
 for i in range(1, len(rules) + 1):
-    t, dt = solve(rules[:i], t, dt)
+    u = 0
+    while True:
+        if all((t + a) % b == 0 for a, b in rules[:i]):
+            if u:
+                t, dt = u, t - u
+                break
+            u = t
+        t += dt
 print(t)
